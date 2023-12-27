8df37a2172ec5dbc652e986e5c4fc8f83368e462
ThienNguyen <ngocthiennguyen2102@gmail.com>
2023-12-27 19:01:13 +0100

    Add TrendingPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TrendComponent } from './trend/trend.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'trending', component: TrendComponent},
 ]; // add export here
